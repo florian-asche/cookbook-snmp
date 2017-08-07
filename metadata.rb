28d88523149b339f5ecae27980deab81c189ba40
Thomas Vincent <thomasvincent@gmail.com>
2017-08-07 00:49:15 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,9 @@
-me 'snmp'
+name 'snmp'
 maintainer 'Thomas Vincent'
 maintainer_email 'thomasvincent@gmail.com'
 license 'Apache-2.0'
-description 'Installs and configures snmpd''
-version '4.0.1'
-
+description 'Installs and configures snmpd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/thomasvincent/cookbook-snmp'
 issues_url 'https://github.com/thomasvincent/cookbook-snmp/issues'
 
@@ -16,4 +15,4 @@ supports 'debian'
 supports 'fedora'
 supports 'redhat'
 supports 'ubuntu'
-Supports 'suse'
+supports 'suse'
